7ca4172f6e1210e6954168fcce6e591b9e596dd1
doldung <69022662+HyunaJo@users.noreply.github.com>
2023-08-04 15:46:41 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -2,23 +2,20 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] dx = {1,-1,0,0};
-    public static int[] dy = {0,0,1,-1};
-    public static int R,C;
+    public static int R,C; // 행, 열
     public static char[][] map;
     public static boolean[][] visited;
+    public static int[] dx = {1,-1,0,0};
+    public static int[] dy = {0,0,1,-1};
+    public static int time = 0;
 
-    public static Queue<Location> waters = new LinkedList<>(); // 물 위치
-    public static Queue<Location> sLocs = new LinkedList<>(); // 고슴도치 위치
-
-    public static final String FAIL = "KAKTUS";
-    public static int time;
+    public static Deque<Location> sDeque = new ArrayDeque<>(); // 고슴도치 위치
+    public static Deque<Location> wDeque = new ArrayDeque<>(); // 물 위치
 
     public static class Location{
         int x;
@@ -29,7 +26,6 @@ public class Main {
             this.y = y;
         }
     }
-
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -44,76 +40,77 @@ public class Main {
             String input = br.readLine();
             for(int j=0;j<C;j++){
                 char inputChar = input.charAt(j);
-                if(inputChar == 'S') {
-                    visited[i][j] = true;
-                    sLocs.add(new Location(i, j));
-                }
-                else if(inputChar == '*') {
-                    visited[i][j] = true;
-                    waters.add(new Location(i, j));
-                }
                 map[i][j] = inputChar;
+                if(inputChar=='S'){
+                    visited[i][j] = true;
+                    sDeque.add(new Location(i,j));
+                }
+                else if(inputChar=='*'){
+                    wDeque.add(new Location(i,j));
+                }
             }
         }
 
         bfs();
         if(time == -1)
-            bw.write(FAIL);
+            bw.write("KAKTUS");
         else
             bw.write(Integer.toString(time));
-        
+
         bw.flush();
-        
+
         br.close();
         bw.close();
+
     }
 
     public static void bfs(){
-        for(time=1;;time++){
-            // 물 차는 것 확인
-            int waterCnt = waters.size();
-            for(int i=0;i<waterCnt;i++){
-                Location nowWater = waters.remove();
+        while(!sDeque.isEmpty()) {
+            time++;
+            // 물 4방향 퍼짐
+            int wDequeSize= wDeque.size();
+            for(int j=0;j<wDequeSize;j++){
+                Location nowWater = wDeque.poll();
 
-                for(int j=0;j<4;j++){
-                    int nx = nowWater.x + dx[j];
-                    int ny = nowWater.y + dy[j];
+                for (int i = 0; i < 4; i++) {
+                    int nx = nowWater.x + dx[i];
+                    int ny = nowWater.y + dy[i];
 
-                    if(nx<0 || ny<0 || nx>=R || ny>=C)
+                    if (nx < 0 || ny < 0 || nx >= R || ny >= C)
                         continue;
 
-                    if(map[nx][ny] == '.'){
+                    if (map[nx][ny] == '.') { // 비어있는 곳인 경우
                         visited[nx][ny] = true;
-                        map[nx][ny] = '*';
-                        waters.add(new Location(nx, ny));
+                        map[nx][ny] = '*'; // 물로 변경시켜주기
+                        wDeque.add(new Location(nx, ny));
                     }
                 }
             }
 
             // 고슴도치 이동
-            int sCnt = sLocs.size();
-            if(sCnt == 0){
-                time = -1;
-                return;
-            }
-            for(int i=0;i<sCnt;i++){
-                Location sNow = sLocs.remove();
+            int sDequeSize = sDeque.size();
+            for(int j=0;j<sDequeSize;j++){
+                Location nowS = sDeque.poll();
 
-                for(int j=0;j<4;j++){
-                    int nx = sNow.x + dx[j];
-                    int ny = sNow.y + dy[j];
+                for (int i = 0; i < 4; i++) {
+                    int nx = nowS.x + dx[i];
+                    int ny = nowS.y + dy[i];
 
-                    if(nx<0 || ny<0 || nx>=R || ny>=C)
+                    if (nx < 0 || ny < 0 || nx >= R || ny >= C)
                         continue;
 
-                    if(map[nx][ny] == 'D')
+                    if (map[nx][ny] == 'D') {
                         return;
-                    if(!visited[nx][ny] && map[nx][ny]=='.'){
+                    }
+
+                    if (map[nx][ny] == '.' && !visited[nx][ny]) {
                         visited[nx][ny] = true;
-                        sLocs.add(new Location(nx, ny));
+                        sDeque.add(new Location(nx, ny));
                     }
                 }
             }
         }
+
+        time = -1;
     }
 }
