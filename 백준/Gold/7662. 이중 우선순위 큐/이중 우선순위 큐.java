bb8396cda89eab84fdfa51e1b1394192b5442cf7
doldung <69022662+HyunaJo@users.noreply.github.com>
2023-09-15 00:07:11 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2640 ms, Memory: 449720 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,23 +2,20 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class Main {
-    public static int T;
+    public static int T; // 테스트케이스의 수
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        T = Integer.parseInt(br.readLine()); // 테스트케이스의 수
+        T = Integer.parseInt(br.readLine());
         while(T-->0){
             int k = Integer.parseInt(br.readLine()); // 연산 개수
             TreeMap<Integer,Integer> nums = new TreeMap<>();
-//            PriorityQueue<Integer> pq = new PriorityQueue<>();
 
             // Q에 대한 연산 수행
             while(k-->0) {
@@ -32,23 +29,18 @@ public class Main {
                     } else {
                         nums.put(num, 1);
                     }
-//                    pq.add(num);
-                } else if (type == 'D') {
-//                    if (pq.isEmpty()) {
-//                        continue;
-//                    }
+                }
+                else if (type == 'D') { // 삭제인 경우
                     if(nums.keySet().isEmpty()){
                         continue;
                     }
 
                     int removedNum = 0;
                     if (num == 1) { // 최댓값 삭제
-//                        removedNum = pq.poll();
                         removedNum = nums.lastKey();
                     }
-                    else{ // 최솟값 삭제
+                    else { // 최솟값 삭제
                         removedNum = nums.firstKey();
-//                        pq.remove(removedNum); // pq에서 제거
                     }
 
                     int numCnt = nums.get(removedNum); // 제거한 값의 기존 개수
@@ -58,14 +50,13 @@ public class Main {
                     else { // 2개 이상인 경우
                         nums.put(removedNum, numCnt - 1); // TreeMap에서 개수 줄이기
                     }
-
                 }
             }
 
-            if(nums.keySet().isEmpty()){
+            if(nums.keySet().isEmpty()) { // 빈 경우
                 bw.write("EMPTY\n");
             }
-            else{
+            else {
                 bw.write(Integer.toString(nums.lastKey())+" "+Integer.toString(nums.firstKey())+"\n");
             }
         }
