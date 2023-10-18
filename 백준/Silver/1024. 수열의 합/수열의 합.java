e58190d12a241852a87428c8db32e1fa31132e59
doldung <69022662+HyunaJo@users.noreply.github.com>
2023-10-18 16:17:41 +0900

    [Silver II] Title: 수열의 합, Time: 4624 ms, Memory: 19324 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -1,53 +1,73 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-    static int N,L;
-    static int min=9999;
-    static int start;
-    public static void main(String[] args) throws IOException {
+    public static int N, L;
+    public static int startNum = -1;
+    public static int minLen = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        
-        for(int i=0; i<N; i++) {
+        N = Integer.parseInt(st.nextToken()); // 목표 합
+        L = Integer.parseInt(st.nextToken()); // 정수 리스트 최소 길이
+
+        for(int i=0;i<N;i++){
+//            long sum = (long)(i+L-1)*(i+L)/2 - (long)i*(i+1)/2 + i; // i~i+L-1까지의 합
             int sum = 0;
             for(int j=i; j<i+L; j++)
                 sum+=j;
-            
-            int cnt = 0;
-            
-            if(sum == N) {
-                for(int j=i; j<i+L; j++)
-                    System.out.print(j+" ");
-                return;
-            }else if(sum > N) {
+
+
+
+            if(sum == N){
+                startNum = i;
+                minLen = L;
                 break;
-            }else {
-                for(int j=i+L; j<N; j++) {
+            }
+            else if(sum > N){ // 불가능
+                break;
+            }
+            else{ // sum < N
+                int len = L; // 수열 길이
+
+                for(int j=i+L;j<N;j++){
+                    if(++len > 100){
+                        break;
+                    }
+
                     sum += j;
-                    cnt++;
-                    if(cnt+L>100)
+                    if(sum > N){
                         break;
-                    if(sum > N)
-                        break;
-                    else if(sum == N) {
-                        if(cnt+L >= L && min > cnt+L) {
-                            min = cnt+L;
-                            start = i;
+                    }
+
+                    if(sum == N){
+                        if(minLen > len){ // 수열 길이가 이전보다 짧은 경우
+                            minLen = len;
+                            startNum = i;
                         }
                     }
                 }
             }
         }
-        if(min == 9999)
-            System.out.println("-1");
-        else {
-            for(int i=start; i<start+min; i++)
-                System.out.print(i+" ");
+
+        if(startNum == -1){
+            bw.write("-1");
         }
-    }    
+        else{
+            for(int i=startNum;i<startNum+minLen;i++){
+                bw.write(Integer.toString(i)+" ");
+            }
+        }
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
 }
