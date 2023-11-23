4bb1d6c8a0ad08af0285c1af5fb28ad784905bdb
doldung <69022662+HyunaJo@users.noreply.github.com>
2023-11-23 19:49:38 +0900

    [level 3] Title: 거스름돈, Time: 55.30 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -3,30 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] money) {
         int moneyCnt = money.length;
-        int[][] dp = new int[moneyCnt][n+1];
+        int[][] dp = new int[moneyCnt+1][n+1];
         
-        Arrays.sort(money);
-        for(int j=1;j<=n;j++){
-            if(j % money[0] == 0){
-                dp[0][j] = 1;
-            }
-        }
-        
-        for(int i=1;i<moneyCnt;i++){
+        for(int i=1;i<=moneyCnt;i++){
             dp[i][0] = 1;
             for(int j=1;j<=n;j++){
-                if(money[i] == j){
-                    dp[i][j] = dp[i-1][j] + 1;
+                dp[i][j] = dp[i-1][j];
+                if (money[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-money[i-1]];
                 }
-                else if (money[i] < j){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-money[i]];
-                }
-                else {
-                    dp[i][j] = dp[i-1][j];
-                }
+                dp[i][j] %= 1_000_000_007;
             }
         }
         
-        return dp[moneyCnt-1][n];
+        return dp[moneyCnt][n];
     }
 }
