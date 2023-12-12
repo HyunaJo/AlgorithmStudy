761eb1617605e03586daac4f3881eb595a9b6f8f
doldung <69022662+HyunaJo@users.noreply.github.com>
2023-12-12 21:10:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        bw.write(Long.toString(A+B+C));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
