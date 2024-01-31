c48d7a4dfbd9832079aa9124819f9ca6775fcb13
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-01-31 16:52:14 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 840 ms, Memory: 118436 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -5,43 +5,45 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N,M;
-    public static int[][] sum;
+	static int N,M;
+	static int[][] sums;
+	static StringBuilder sb = new StringBuilder(); // 결과
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // NxN 크기의 표
+		M = Integer.parseInt(st.nextToken()); // 구해야 하는 횟수
+		
+		sums = new int[N+1][N+1]; // (1,1)부터 해당 칸까지의 합을 담을 배열
+		// 합 담을 배열 채우기
+		for(int i=1;i<=N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1;j<=N;j++) {
+				int num = Integer.parseInt(st.nextToken()); // 표에서의 (i,j) 값
+				// (1,1) ~ (i,j) 구간 합 구하기
+				sums[i][j] = num + sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1];
+			}
+		}
+		
+		while(M-->0) { // M만큼 반복
+			// (x1, y1),(x2, y2) 입력 받기
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			int answer = sums[x2][y2] - sums[x1-1][y2] - sums[x2][y1-1] + sums[x1-1][y1-1];
+			sb.append(answer).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        sum = new int[N+1][N+1];
-        for(int i=1;i<=N;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=1;j<=N;j++){
-                sum[i][j] = sum[i][j-1]+Integer.parseInt(st.nextToken()); // sum[i][j] = (i,0)~(i,j)까지의 합
-            }
-        }
-
-        while(M-->0){
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            int ans = 0;
-            for(int i=x1;i<=x2;i++){
-                ans += (sum[i][y2]-sum[i][y1-1]);
-            }
-
-            bw.write(Integer.toString(ans)+"\n");
-        }
-
-        bw.flush();
-
-        br.close();
-        bw.close();
-    }
 }
