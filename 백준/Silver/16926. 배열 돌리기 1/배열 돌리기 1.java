9cfe248f42d292028334e65504f120355d595458
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-02-06 22:14:57 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 804 ms, Memory: 30524 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -5,64 +5,71 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N,M,rotCnt;
-    public static int[][] nums;
-
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        rotCnt = Integer.parseInt(st.nextToken());
-
-        nums = new int[N][M];
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;j++){
-                nums[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        while(rotCnt-->0){
-            rotateMatrix();
-        }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                bw.write(nums[i][j]+" ");
-            }
-            bw.write("\n");
-        }
-        bw.flush();
-
-        br.close();
-        bw.close();
-    }
-
-    public static void rotateMatrix(){
-        int rotateLineCnt = Math.min(N,M)/2;
-        for(int i=0;i<rotateLineCnt;i++){
-            int tmp = nums[i][i];
-
-            for(int j=i;j<M-i-1;j++){ // 왼쪽으로 이동
-                nums[i][j] = nums[i][j+1];
-            }
-
-            for(int j=i;j<N-i-1;j++){ // 위로 이동
-                nums[j][M-i-1] = nums[j+1][M-i-1];
-            }
-
-            for(int j=M-i-1;j>i;j--){ // 오른쪽으로 이동
-                nums[N-i-1][j] = nums[N-i-1][j-1];
-            }
-
-            for(int j=N-i-1;j>i;j--){ // 아래쪽으로 이동
-                nums[j][i] = nums[j-1][i];
-            }
-
-            nums[i+1][i] = tmp;
-        }
-    }
+	static int N,M,R;
+	static int[][] arr;
+	static int[][] next = {{0,1},{1,0},{0,-1},{-1,0}};
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N][M];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int borderCnt = Math.min(N, M)/2;
+		while(R-->0) {
+			for(int i=0;i<borderCnt;i++) {
+				rotateBorder(i);
+			}
+		}
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				sb.append(arr[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	
+	public static void rotateBorder(int start) {
+		int tmp = arr[start][start];
+		int endCol = M-start-1;
+		int endRow = N-start-1;
+		
+		int x = start;
+		int y = start;
+		for(int i=0;i<4;i++) {
+			while(true) {
+				int nx = x + next[i][0];
+				int ny = y + next[i][1];
+				
+				if(nx<start || nx>endRow || ny<start || ny>endCol) {
+					break;
+				}
+				
+				if(nx==start && ny==start) {
+					break;
+				}
+				
+				arr[x][y] = arr[nx][ny];
+				x = nx;
+				y = ny;
+			}
+		}
+		arr[x][y] = tmp;
+	}
 }
