c39c521599511d26355b3adfe18ada9f88fd6a54
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-02-29 10:41:12 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -13,26 +13,15 @@ public class Main {
 
     static int N;
     static int[][] map;
+    static int[][][] possibleCnt;
     static int ans;
 
-
-    static class Location{
-        int x;
-        int y;
-
-        public Location(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
         map = new int[N+1][N+1];
-
         for(int i=1;i<=N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=1;j<=N;j++){
@@ -40,86 +29,49 @@ public class Main {
             }
         }
 
-        movePipe(new Location(1,1), new Location(1,2));
+        possibleCnt = new int[N+1][N+1][3];
+        possibleCnt[1][2][HORIZONTAL] = 1;
+        
+        for(int i=1;i<=N;i++) {
+        	for(int j=1;j<=N;j++) {
+        		if(possibleCnt[i][j][HORIZONTAL]==0 && possibleCnt[i][j][VERTICAL]==0 && possibleCnt[i][j][DIAGONAL]==0) {
+        			continue;
+        		}
+        		
+        		if(isPossible(i, j+1)) {
+        			possibleCnt[i][j+1][HORIZONTAL] += possibleCnt[i][j][HORIZONTAL];
+        			possibleCnt[i][j+1][HORIZONTAL] += possibleCnt[i][j][DIAGONAL];
+        		}
+        		
+        		if(isPossible(i+1, j)) {
+        			possibleCnt[i+1][j][VERTICAL] += possibleCnt[i][j][VERTICAL];
+        			possibleCnt[i+1][j][VERTICAL] += possibleCnt[i][j][DIAGONAL];
+        		}
+        		
+        		if(isPossible(i, j+1) && isPossible(i+1, j) && isPossible(i+1, j+1)) {
+        			possibleCnt[i+1][j+1][DIAGONAL] += possibleCnt[i][j][HORIZONTAL];
+        			possibleCnt[i+1][j+1][DIAGONAL] += possibleCnt[i][j][VERTICAL];
+        			possibleCnt[i+1][j+1][DIAGONAL] += possibleCnt[i][j][DIAGONAL];
+        		}
+        	}
+        }
+        
+        
+        for(int i=0;i<3;i++){
+            ans += possibleCnt[N][N][i];
+        }
         System.out.println(ans);
     }
 
-    public static void movePipe(Location left, Location right){
-        if(right.x==N && right.y==N){
-            ans++;
-            return;
-        }
-
-        int type = getType(left, right);
-        int nx = right.x+1;
-        int ny = right.y+1;
-
-        if(type == HORIZONTAL){
-            if(ny>N){
-                return;
-            }
-
-            left.y += 1;
-
-            if(map[right.x][ny]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(right.x,ny));
-            }
-
-            if(nx<=N && map[nx][ny]==BLANK && map[right.x][ny]==BLANK && map[nx][right.y]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(nx,ny));
-            }
-
-            return;
-        }
-
-        if(type == VERTICAL){
-            if(nx>N){
-                return;
-            }
-
-            left.x += 1;
-
-            if(map[nx][right.y]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(nx,right.y));
-            }
-
-            if(ny<=N && map[nx][ny]==BLANK && map[right.x][ny]==BLANK && map[nx][right.y]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(nx,ny));
-            }
-
-            return;
-        }
-
-        if(type == DIAGONAL){
-            left.x += 1;
-            left.y += 1;
-
-            if(ny<=N && map[right.x][ny]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(right.x,ny));
-            }
-
-            if(nx<=N && map[nx][right.y]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(nx,right.y));
-            }
-
-            if(nx<=N && ny<=N && map[nx][ny]==BLANK && map[right.x][ny]==BLANK && map[nx][right.y]==BLANK){
-                movePipe(new Location(left.x,left.y),new Location(nx,ny));
-            }
-        }
-    }
-
-    public static int getType(Location left, Location right){
-        int xDiff = Math.abs(left.x - right.x);
-        int yDiff = Math.abs(left.y - right.y);
-
-        if(xDiff==yDiff){ // 대각선
-            return DIAGONAL;
-        }
-
-        if(xDiff==1){ // 세로
-            return VERTICAL;
-        }
-
-        return HORIZONTAL; // 가로
+    public static boolean isPossible(int x, int y) {
+    	if(x<1 || x>N || y<1 || y>N) {
+    		return false;
+    	}
+    	
+    	if(map[x][y] == 1) {
+    		return false;
+    	}
+    	
+    	return true;
     }
 }
