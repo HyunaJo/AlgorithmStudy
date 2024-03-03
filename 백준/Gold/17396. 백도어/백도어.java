4a769704dd1346861cc03333aa6ea270affa0aa9
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-03-03 20:58:16 +0900

    [Gold V] Title: 백도어, Time: 996 ms, Memory: 138704 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.java
+++ 백준/Gold/17396. 백도어/백도어.java
@@ -2,91 +2,130 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    static final long MAX = 100_000L *100_000+1;
-    static int N,M;
-    static int[] isVisible;
-    static long[] times;
-    static ArrayList<Edge>[] edges;
 
-    static class Edge implements Comparable<Edge>{
-        int target;
-        long time;
+    static class Vertex implements Comparable<Vertex> {
+        int index;
+        long weight;
 
-        public Edge(int target, long time){
-            this.target = target;
-            this.time = time;
+        public Vertex(int index, long weight) {
+            this.index = index;
+            this.weight = weight;
         }
 
-        public int compareTo(Edge o){
-            return (int)(this.time - o.time);
+        @Override
+        public int compareTo(Vertex v) {
+            return Long.compare(this.weight, v.weight);
         }
     }
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stn;
+    static int N, M, from, to;
+    static long weight;
+    static boolean[] sight, isVisited;
+    static List<Vertex>[] adjList;
+    static long[] minWeight;
+    static PriorityQueue<Vertex> pqueue = new PriorityQueue<>();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        stn = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        isVisible = new int[N];
-        times = new long[N];
-        edges = new ArrayList[N];
-        for(int i=0;i<N;i++){
-            isVisible[i] = Integer.parseInt(st.nextToken());
-            times[i] = MAX;
-            edges[i] = new ArrayList<>();
-        }
+        N = Integer.parseInt(stn.nextToken());
+        M = Integer.parseInt(stn.nextToken());
 
-        for(int i=0;i<M;i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int target = Integer.parseInt(st.nextToken());
-            long time = Long.parseLong(st.nextToken());
-
-            edges[start].add(new Edge(target,time));
-            edges[target].add(new Edge(start,time));
-        }
-
-        dijkstra(0);
-        bw.write((times[N-1]==MAX)?"-1":Long.toString(times[N-1]));
-        bw.flush();
-
-        br.close();
-        bw.close();
-    }
-
-    public static void dijkstra(int start){
-        times[start] = 0;
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start,0));
-
-        while(!pq.isEmpty()){
-            Edge now = pq.poll();
-
-            if(now.time > times[now.target]){
+        minWeight = new long[N];
+        isVisited = new boolean[N];
+        sight = new boolean[N];
+        stn = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            if (Integer.parseInt(stn.nextToken()) == 0) {
                 continue;
             }
+            sight[i] = true;
+        }
 
-            for(Edge next:edges[now.target]){
-                if(isVisible[next.target]==1 && next.target!=N-1){
+        adjList = new List[N];
+        for (int i = 0; i < N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            stn = new StringTokenizer(br.readLine());
+
+            from = Integer.parseInt(stn.nextToken());
+            to = Integer.parseInt(stn.nextToken());
+            weight = Long.parseLong(stn.nextToken());
+
+            adjList[from].add(new Vertex(to, weight));
+            adjList[to].add(new Vertex(from, weight));
+        }
+
+        Arrays.fill(minWeight, Long.MAX_VALUE);
+        minWeight[0] = (long)0;
+        isVisited[0] = true;
+        for (Vertex nextVertex : adjList[0]) {
+            if (sight[nextVertex.index] && !(nextVertex.index == N - 1)) {
+                continue;
+            }
+            if (isVisited[nextVertex.index]) {
+                continue;
+            }
+            minWeight[nextVertex.index] = nextVertex.weight;
+            pqueue.add(nextVertex);
+        }
+
+        while (!pqueue.isEmpty()) {
+            Vertex nearVertex = pqueue.poll();
+
+            if (sight[nearVertex.index] && !(nearVertex.index == N - 1)) {
+                continue;
+            }
+            if (isVisited[nearVertex.index]) {
+                continue;
+            }
+//            if (minWeight[nearVertex.index] < nearVertex.weight) {
+//                continue;
+//            }
+            isVisited[nearVertex.index] = true;
+//            minWeight[nearVertex.index] = nearVertex.weight;
+//            if (nearVertex.index == N - 1) {
+//                break;
+//            }
+            for (Vertex nextVertex : adjList[nearVertex.index]) {
+                if (isVisited[nextVertex.index]) {
                     continue;
                 }
-
-                if(next.time+now.time >= times[next.target]){
+                if (minWeight[nextVertex.index] < minWeight[nearVertex.index] + nextVertex.weight) {
                     continue;
                 }
+                if (sight[nextVertex.index] && !(nextVertex.index == N - 1)) {
+                    continue;
+                }
+                minWeight[nextVertex.index] = minWeight[nearVertex.index] + nextVertex.weight;
+                pqueue.add(new Vertex(nextVertex.index,minWeight[nextVertex.index]));
 
-                times[next.target] = next.time+now.time;
-                pq.add(new Edge(next.target, times[next.target]));
             }
         }
+
+        if (minWeight[N - 1] == Long.MAX_VALUE) {
+            bw.write(Integer.toString(-1));
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
+        }
+        bw.write(Long.toString(minWeight[N - 1]));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
