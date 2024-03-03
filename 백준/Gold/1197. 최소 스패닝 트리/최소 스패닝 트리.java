a9446b3a668edd7ff23fef3841e0536ef188aa6b
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-03-03 19:31:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 556 ms, Memory: 54448 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,32 +3,30 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int V,E;
-    static ArrayList<Edge> edges = new ArrayList<>();
-    static int[] parents;
+    static ArrayList<Edge>[] edges;
+    static int[] minWeights;
 
     static class Edge implements Comparable<Edge>{
-        int from;
         int to;
         int weight;
 
-        public Edge(int from, int to, int weight){
-            this.from = from;
+        public Edge(int to, int weight){
             this.to = to;
             this.weight = weight;
         }
 
         @Override
         public int compareTo(Edge o){
-            return Integer.compare(this.weight, o.weight);
+            return Integer.compare(this.weight,o.weight);
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -36,17 +34,22 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
+        edges = new ArrayList[V+1];
+        minWeights = new int[V+1];
+        for(int i=1;i<=V;i++){
+            edges[i] = new ArrayList<>();
+            minWeights[i] = Integer.MAX_VALUE;
+        }
+
         for(int i=0;i<E;i++){
             st = new StringTokenizer(br.readLine());
-            edges.add(new Edge(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            edges[from].add(new Edge(to,weight));
+            edges[to].add(new Edge(from,weight));
         }
 
-        parents = new int[V+1];
-        for(int i=1;i<=V;i++){
-            parents[i] = i;
-        }
-
-        Collections.sort(edges);
         bw.write(Integer.toString(findMin()));
         bw.flush();
 
@@ -55,44 +58,44 @@ public class Main {
     }
 
     public static int findMin(){
-        int weightSum = 0;
-        int cnt = 0;
+        boolean[] visited = new boolean[V+1];
 
-        for(Edge edge:edges){
-            if(!union(edge.from,edge.to)){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(1,0));
+        minWeights[1] = 0;
+
+        int cnt = 0;
+        int sum = 0;
+        while(!pq.isEmpty()){
+            Edge now = pq.poll();
+
+            if(visited[now.to]){
                 continue;
             }
 
-            weightSum += edge.weight;
+            visited[now.to] = true;
+            sum += now.weight;
             if(++cnt == V){
                 break;
             }
+
+            ArrayList<Edge> nexts = edges[now.to];
+            for(Edge next:nexts){
+                if(visited[next.to]){
+                    continue;
+                }
+
+                if(minWeights[next.to] <= next.weight){
+                    continue;
+                }
+
+                minWeights[next.to] = next.weight;
+                pq.add(new Edge(next.to, next.weight));
+            }
         }
 
-        return weightSum;
-    }
-
-    public static boolean union(int a, int b){
-        a = findParent(a);
-        b = findParent(b);
-
-        if(a == b){
-            return false;
-        }
-
-        if(a<b){
-            parents[b] = a;
-            return true;
-        }
-        parents[a] = b;
-        return true;
-    }
-
-    public static int findParent(int a){
-        if(a == parents[a]){
-            return a;
-        }
-
-        return parents[a] = findParent(parents[a]);
+        return sum;
     }
 }
+
+
