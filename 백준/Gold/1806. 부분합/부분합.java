77155d4cda4fb1f4ec8636cd17cd3340cd6eb9f5
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-03-03 22:14:21 +0900

    [Gold IV] Title: 부분합, Time: 312 ms, Memory: 23908 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -5,65 +5,60 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int N, S;
-	public static int[] arr;
-	public static int ans = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		S = Integer.parseInt(st.nextToken());
-		
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		findAnswer();
-		
-		if(ans == Integer.MAX_VALUE)
-			ans = 0;
-		bw.write(Integer.toString(ans));
-		bw.flush();
-		
-		br.close();
-		bw.close();
-	}
-	
-	public static void findAnswer() {
-		int lp = 0; // 왼쪽 포인터
-		int rp = 0; // 오른쪽 포인터
-		long sum = arr[0];
-		
-		while(true) {
-			if(sum < S) { // 합이  S보다 작은 경우
-				rp++; // 오른쪽 포인터 오른쪽으로 이동
-				if(rp==N)
-					return;
-				sum += arr[rp];
-			}
-			else { // 합이 S이상인 경우
-				if((rp-lp+1)<ans)
-					ans = rp-lp+1;
-				
-				if(lp==rp) {
-					sum -= arr[lp];
-					lp++;
-					rp++;
-					if(rp==N)
-						return;
-					sum += arr[rp];
-				}
-				else {
-					sum -= arr[lp];
-					lp++;
-				}
-			}
-		}
-		
-	}
+    static int N,S;
+    static int[] nums;
+    static int min;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        min = Integer.MAX_VALUE;
+        findMin();
+
+        bw.write((min==Integer.MAX_VALUE)?"0":Integer.toString(min));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static void findMin(){
+        int lp = 0;
+        int rp = 0;
+        int sum = nums[lp];
+
+        while(lp<N){
+            if(sum < S){
+                if(++rp == N){
+                    break;
+                }
+                sum += nums[rp];
+                continue;
+            }
+
+            min = Math.min(min, rp-lp+1);
+            if(lp == rp){
+                sum -= nums[lp++];
+                if(++rp == N){
+                    break;
+                }
+                sum += nums[rp];
+                continue;
+            }
+
+            sum -= nums[lp++];
+        }
+
+    }
 }
