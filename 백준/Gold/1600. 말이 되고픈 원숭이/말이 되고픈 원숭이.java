ba8baa01f5b29144690d98f94b5931ae656aa10e
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-03-03 15:27:38 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 456 ms, Memory: 63724 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,104 +1,125 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-	static int K, W, H;
-	static char[][] map;
-	static int[][][] moveMap;
-	static int[] dr = {-1, 1, 0, 0, -1, -2, -2, -1, 1, 2,  2,  1};
-	static int[] dc = {0, 0, -1, 1, -2, -1,  1,  2, 2, 1, -1, -2};
+    static final int BLANK = 0;
+    static final int OBSTACLE = 1;
 
-	public static void main(String[] args) throws Exception {
-		
-		K = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		
-		map = new char[H+1][W+1];
-		for (int i=1; i<=H; i++) {
-			String line = br.readLine();
-			for (int j=0; j<W; j++) {
-				map[i][j+1] = line.charAt(j<<1);
-			}
-		}
-		
-		moveMap = new int[K+1][H+1][W+1];
-		for (int[][] dm : moveMap) {
-			for (int[] r : dm) {
-				Arrays.fill(r, Integer.MAX_VALUE);
-			}
-		}
-		for (int i=0; i<=K; i++) {
-			moveMap[i][1][1] = 0;
-		}
-		
-		Queue<Coord> queue = new ArrayDeque<>();
-		queue.add(new Coord(1, 1, 0, 0));
-		
-		while (!queue.isEmpty()) {
-			Coord c = queue.poll();
-			if (c.r == H && c.c == W) {
-				break;
-			}
-			
-			for (int d=0; d<4; d++) {
-				int nr = c.r+dr[d];
-				int nc = c.c+dc[d];
-				
-				if (isOutofBound(nr, nc) || map[nr][nc] == '1') {
-					continue;
-				}
-				if (moveMap[c.jc][nr][nc] <= c.mc+1) {
-					continue;
-				}
-				
-				moveMap[c.jc][nr][nc] = c.mc + 1;
-				queue.add(new Coord(nr, nc, moveMap[c.jc][nr][nc], c.jc));
+    static int K,W,H;
+    static int[][] map;
+    static int[][][] movings;
+    static int[][] horseDirections = {{-2,-1},{-1,-2},{-2,1},{-1,2},{2,-1},{1,-2},{2,1},{1,2}};
+    static int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
 
-			}
-			
-			// 말 이동
-			if (c.jc == K) {
-				continue;
-			}
-			for (int d=4; d<12; d++) {
-				int nr = c.r+dr[d];
-				int nc = c.c+dc[d];
+    static class Location{
+        int x;
+        int y;
+        int k;
+        int movingCnt;
 
-				if (isOutofBound(nr, nc) || map[nr][nc] == '1') {
-					continue;
-				}
-				if (moveMap[c.jc+1][nr][nc] <= c.mc+1) {
-					continue;
-				}
-				
-				moveMap[c.jc+1][nr][nc] = c.mc + 1;
-				queue.add(new Coord(nr, nc, moveMap[c.jc+1][nr][nc], c.jc+1));
-				
-			}
-		}
-		
-		int minMc = Integer.MAX_VALUE;
-		for (int i=0; i<=K; i++) {
-			minMc = Math.min(minMc, moveMap[i][H][W]);
-		}
-		System.out.println(minMc == Integer.MAX_VALUE ? -1 : minMc);
-	}
-	public static boolean isOutofBound(int r, int c) {
-		return r<1 || H<r || c<1 || W<c;
-	}
+        public Location(int x, int y, int k, int movingCnt){
+            this.x = x;
+            this.y = y;
+            this.k = k;
+            this.movingCnt = movingCnt;
+        }
+    }
 
-	static class Coord {
-		int r, c, mc, jc;
-		public Coord (int r, int c, int mc, int jc) {
-			this.r = r;
-			this.c = c;
-			this.mc = mc;
-			this.jc = jc;
-		}
-	}
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        K = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        map = new int[H][W];
+        for(int i=0;i<H;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<W;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        movings = new int[H][W][K+1];
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W;j++){
+                Arrays.fill(movings[i][j],Integer.MAX_VALUE);
+            }
+        }
+        System.out.println(findMin());
+    }
+
+    public static int findMin(){
+        if(W==1 && H==1){
+            return 0;
+        }
+
+        Deque<Location> dq = new ArrayDeque<>();
+        dq.add(new Location(0,0,0,0));
+
+        while(!dq.isEmpty()){
+            Location now = dq.poll();
+            if(now.x==H-1 && now.y==W-1){
+                return now.movingCnt;
+            }
+
+            if(now.k < K){
+                for(int i=0;i<8;i++){
+                    int nx = now.x + horseDirections[i][0];
+                    int ny = now.y + horseDirections[i][1];
+
+                    if(!isPossible(nx,ny)){
+                        continue;
+                    }
+
+                    if(movings[nx][ny][now.k+1] <= now.movingCnt+1){
+                        continue;
+                    }
+
+                    movings[nx][ny][now.k+1] = now.movingCnt+1;
+                    dq.add(new Location(nx, ny,now.k+1,now.movingCnt+1));
+                }
+            }
+
+            for(int i=0;i<4;i++){
+                int nx = now.x + directions[i][0];
+                int ny = now.y + directions[i][1];
+
+                if(!isPossible(nx,ny)){
+                    continue;
+                }
+
+                if(movings[nx][ny][now.k] <= now.movingCnt+1){
+                    continue;
+                }
+
+                movings[nx][ny][now.k] = now.movingCnt+1;
+                dq.add(new Location(nx, ny,now.k,now.movingCnt+1));
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static boolean isPossible(int nx, int ny){
+        if(nx<0 || nx>=H || ny<0 || ny>=W){
+            return false;
+        }
+
+        if(map[nx][ny] == OBSTACLE){
+            return false;
+        }
+
+        return true;
+    }
 }
