6be6e3ff03e0a57a034fed9d1087a87a0bb008e0
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-03-21 20:00:04 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 928 ms, Memory: 111052 KB -BaekjoonHub

--- 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -8,76 +8,90 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N, M;
-    public static HashMap<String, HashSet<String>> folders;
-    public static HashMap<String, HashSet<String>> files;
-    public static ArrayList<String> answers;
+    static final int FOLDER = 1;
+    static final int FILE = 0;
+    static int N,M;
+    static HashMap<String, Integer> folderIdxMap;
+    static Folder[] folders;
+    static HashSet<String> fileNames;
+    static int fileTotalCnt, fileTypeCnt;
+    static StringBuilder sb = new StringBuilder();
+
+    static class Folder{
+        ArrayList<String> subfolders;
+        HashSet<String> subfiles;
+        int subfileCnt;
+
+        public Folder(){
+            subfolders = new ArrayList<>();
+            subfiles = new HashSet<>();
+        }
+    }
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 폴더의 총 개수
-        M = Integer.parseInt(st.nextToken()); // 파일의 총 개수
-        folders = new HashMap<>();
-        files = new HashMap<>();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        folderIdxMap = new HashMap<>();
+        folders = new Folder[N+1];
+        int folderIdx = 0;
 
         for(int i=0;i<N+M;i++){
             st = new StringTokenizer(br.readLine());
             String P = st.nextToken(); // 상위 폴더 이름
             String F = st.nextToken(); // 폴더 또는 파일 이름
-            int C = Integer.parseInt(st.nextToken()); // 파일(0)인지 폴더(1)인지
+            int C = Integer.parseInt(st.nextToken()); // 폴더인지 파일인지
 
-            if(C==0){ // 파일
-                if(!files.containsKey(P)){
-                    files.put(P,new HashSet<>());
-                }
-
-                files.get(P).add(F);
+            // 상위 폴더 인덱스
+            int idx = folderIdxMap.getOrDefault(P,-1);
+            if(idx == -1){
+                idx = folderIdx;
+                folderIdxMap.put(P, folderIdx);
+                folders[folderIdx++] = new Folder();
             }
-            else{ // 폴더
-                if(!folders.containsKey(P)){
-                    folders.put(P,new HashSet<>());
-                }
 
-                folders.get(P).add(F);
+            if(C == FILE){ // 파일인 경우
+                folders[idx].subfiles.add(F);
+                folders[idx].subfileCnt++;
+                continue;
             }
+
+            // 폴더인 경우
+            folders[idx].subfolders.add(F);
         }
 
-        answers = new ArrayList<>();
         int Q = Integer.parseInt(br.readLine());
         while(Q-->0){
-            String path = br.readLine();
-            String[] pathFolders = path.split("/");
+            String[] inputs = br.readLine().split("/");
+            String target = inputs[inputs.length-1];
 
-
-            HashSet<String> subFiles = new HashSet<>();
-            int fileCnt = dfs(pathFolders[pathFolders.length-1], 0, subFiles);
-            bw.write(subFiles.size()+" "+fileCnt+"\n");
+            fileNames = new HashSet<>();
+            fileTotalCnt = 0;
+            dfs(target);
+            fileTypeCnt = fileNames.size();
+            sb.append(fileTypeCnt).append(" ").append(fileTotalCnt).append("\n");
         }
+        bw.write(sb.toString());
         bw.flush();
-
-        br.close();
-        bw.close();
     }
 
-    public static int dfs(String folderName, int fileCnt, HashSet<String> subFiles){
-        HashSet<String> subFolders = folders.get(folderName);
+    public static void dfs(String folderName){
+        int idx = folderIdxMap.getOrDefault(folderName,-1);
 
-        if(files.containsKey(folderName)){
-            for(String subFile:files.get(folderName)){
-                subFiles.add(subFile);
-                fileCnt++;
-            }
+        if(idx == -1){
+            return;
         }
 
-        if(folders.containsKey(folderName)){
-            for(String subFolder:subFolders){
-                fileCnt = dfs(subFolder, fileCnt, subFiles);
-            }
-        }
+        Folder folder = folders[idx];
+        fileTotalCnt += folder.subfileCnt;
+        fileNames.addAll(folder.subfiles);
 
-        return fileCnt;
+        for(String subfolder:folder.subfolders){
+            dfs(subfolder);
+        }
     }
 }
