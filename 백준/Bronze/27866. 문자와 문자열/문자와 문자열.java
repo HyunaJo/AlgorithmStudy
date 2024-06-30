da98d4ad93018b46becdfe061b377d924038e811
doldung <69022662+HyunaJo@users.noreply.github.com>
2024-06-30 16:38:51 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static String S;
+    static int i;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        S = br.readLine();
+        i = Integer.parseInt(br.readLine());
+        bw.write(S.charAt(i-1));
+        bw.flush();
+    }
+}
